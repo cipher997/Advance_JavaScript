36786d6c17abb8ba57fd4d91f968b00aa494c6c2
Kartik! <kartik97gaikwad@gmail.com>
2022-12-15 13:28:36 +0530

    mobile.js

---
+++ Modules/2. Default Function Import Export/mobile.js
@@ -0,0 +1,19 @@
+// Default Export Function
+export default function show() {
+  console.log("Hello Module");
+}
+
+/*
+function show() {
+  console.log("Hello Module");
+}
+export default show;
+
+*/
+
+// <!-- These codes are created, for those who have immense interest in coding and
+// programming as me.. everything is open to you people out there on my personal Github
+// account (https://github.com/cipher997) for better understanding of advanced javascript in
+// terms of my personal understandings and learnings. If you have any doubts please feel free to
+// connect me (email-id:kartik97gaikwad@gmail.com) regarding general coding
+// informational purposes! -->
