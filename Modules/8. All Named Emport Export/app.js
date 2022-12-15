f0b68d0ecb16302dc06e531d6e7441ce5fb41987
Kartik! <kartik97gaikwad@gmail.com>
2022-12-15 13:38:00 +0530

    app.js

---
+++ Modules/8. All Named Emport Export/app.js
@@ -0,0 +1,13 @@
+// All Named Import
+import * as device from "./mobile.js";
+const n = new device.Nokia();
+n.VolumnUp();
+device.show();
+console.log(device.a);
+
+// <!-- These codes are created, for those who have immense interest in coding and
+// programming as me.. everything is open to you people out there on my personal Github
+// account (https://github.com/cipher997) for better understanding of advanced javascript in
+// terms of my personal understandings and learnings. If you have any doubts please feel free to
+// connect me (email-id:kartik97gaikwad@gmail.com) regarding general coding
+// informational purposes! -->
